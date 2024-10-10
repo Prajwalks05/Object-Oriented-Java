b37aa9e512606c1891f296963c6bab757e6b00ff
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-10-10 09:15:44 +0530

    program to perform static array declaration

---
+++ Array.java
@@ -0,0 +1,24 @@
+
+public class array {
+
+    public static void main(String[] args) {
+        int days[] = new int[12];
+        days[0] = 31;
+        days[1] = 28;
+        days[2] = 31;
+        days[3] = 30;
+        days[4] = 31;
+        days[5] = 30;
+        days[6] = 31;
+        days[7] = 31;
+        days[8] = 30;
+        days[9] = 31;
+        days[10] = 30;
+        days[11] = 31;
+        for(int i=0;i<12;i++)
+        {
+            System.out.println(+days[i]);
+        }
+    }
+
+}
