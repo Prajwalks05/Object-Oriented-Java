81cae917227130d4d59149c1e8cab4b54f0270c2
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-10-10 13:55:15 +0530

    program to demonstrate passing arguments through command line at run time

---
+++ cmd_args.java
@@ -0,0 +1,9 @@
+public class cmd_args {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("command line args" + args[i]);
+        }
+    }
+
+}
