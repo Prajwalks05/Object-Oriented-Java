44277ab20bf9092fcf4d60571ffef24374194708
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-10-21 15:55:35 +0530

    Create Bank_det.java

---
+++ Lab Programs/Bank_det.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class Account
+{	
+  Account()
+	{
+		Scanner sc=new Scanner(System.in);
+		String coustomer_name;
+		String account_name;
+		int initial_dep;
+	}
+}
+class Savings_Acc extends Account
+{
+    void cal_compound()
+    {
+        
+    }
+    void withdraw()
+
+    {
+        
+    }
+}
+class Current_Acc extends Account
+{
+    void service_charge()
+    {
+        
+    }
+    
+}
+class Bank
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
