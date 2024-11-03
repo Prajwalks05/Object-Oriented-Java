3ba12f28b2b6eca223d780039b44d13dfc7e474c
Prajwalks05 <prajwalks.cs24@bmsce.ac.in>
2024-11-03 21:15:47 +0530

    program to autobox and unbox

---
+++ boxing.java
@@ -0,0 +1,7 @@
+public class wrap {
+    public static void main(String args[]) {
+        Integer iob=Integer.valueOf(100);//boxing
+        int i=iob.intValue();
+        System.out.println("Int value is:"+i);
+    }
+}
